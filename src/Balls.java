e449b9b8857fa51997fd005cabc8708ff4b2a0c9
Claire <claire.deshayes71@gmail.com>
2017-10-17 16:07:25 +0200

    creation des trois premières classes - répondu à la question 2

---
+++ src/Balls.java
@@ -0,0 +1,29 @@
+import java.awt.Point;
+
+public class Balls {
+	public Point p1 = new Point(5,8);
+	public Point p2 = new Point(21,1);
+	public Point p3 = new Point(0,4);
+	public Point p4 = new Point(4,8);
+	
+	public void translate(int dx, int dy) {
+		p1.translate(dx, dy);
+		p2.translate(dx, dy);
+		p3.translate(dx, dy);
+		p4.translate(dx, dy);
+	}
+	public void reInit() {
+		p1.setLocation(5, 8);
+		p2.setLocation(21, 1);
+		p3.setLocation(0, 4);
+		p4.setLocation(4, 8);
+	}
+	
+	@Override
+	public String toString() {
+		return "p1("+p1.getX()+";"+p1.getY()+"), p2("+p2.getX()+";"+p2.getY()+"), p3("+p3.getX()+";"+p3.getY()+"), p4("+p4.getX()+";"+p4.getY()+")";
+	}
+	
+	
+	
+}
---
+++ src/BallsSimulator.java
@@ -0,0 +1,13 @@
+import gui.Simulable;
+
+public class BallsSimulator implements Simulable {
+	@Override
+	public void next() {
+		
+	}
+	
+	@Override
+	public void restart() {
+		
+	}
+}
