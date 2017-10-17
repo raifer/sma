ce1e8e4ecd41f8d2be9d737ecfb599608414a655
Mathieu Barbe <mat.barbe@gmail.com>
2017-10-17 17:00:36 +0200

    Ajout constructeur dans Balls

--- src/Balls.java
+++ src/Balls.java
@@ -1,10 +1,17 @@
 import java.awt.Point;
 
 public class Balls {
-	public Point p1 = new Point(5,8);
-	public Point p2 = new Point(21,1);
-	public Point p3 = new Point(0,4);
-	public Point p4 = new Point(4,8);
+    private Point p1; 
+    private Point p2; 
+    private Point p3; 
+    private Point p4;
+    
+	public Balls(){
+	    p1 = new Point(5,8);
+		p2 = new Point(21,1);
+		p3 = new Point(0,4);
+		p4 = new Point(4,8);
+	}
 	
 	public void translate(int dx, int dy) {
 		p1.translate(dx, dy);
@@ -12,6 +19,7 @@ public class Balls {
 		p3.translate(dx, dy);
 		p4.translate(dx, dy);
 	}
+	
 	public void reInit() {
 		p1.setLocation(5, 8);
 		p2.setLocation(21, 1);
