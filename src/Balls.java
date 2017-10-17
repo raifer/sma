e5506b9bed3d80f2d8e91dca70894a149de9bb23
Mathieu Barbe <mat.barbe@gmail.com>
2017-10-17 21:02:01 +0200

    Mise au propre de Ball :
    * Passage en mode tableau
    * Un pour les Balls et un pour les BallsOri
    * UilisationUtilisation de copie avec Point.getLocation()

--- src/Balls.java
+++ src/Balls.java
@@ -1,37 +1,46 @@
 import java.awt.Point;
 
 public class Balls {
-    private Point p1; 
-    private Point p2; 
-    private Point p3; 
-    private Point p4;
+    private Point [] balls, ballsOri;
     
 	public Balls(){
-	    p1 = new Point(5,8);
-		p2 = new Point(21,1);
-		p3 = new Point(0,4);
-		p4 = new Point(4,8);
+	    this.balls = new Point[4];
+	    this.ballsOri = new Point[4];
+	    
+	    this.balls[0] = new Point(5,8);
+	    this.balls[1] = new Point(21,1);
+	    this.balls[2] = new Point(0,4);
+	    this.balls[3] = new Point(4,8);
+	    
+	    for(int i=0; i < balls.length; i++){
+		//this.ballsOri[i] = (Point)this.balls[i].clone();
+		this.ballsOri[i] = this.balls[i].getLocation();
+	    }
 	}
 	
 	public void translate(int dx, int dy) {
-		p1.translate(dx, dy);
-		p2.translate(dx, dy);
-		p3.translate(dx, dy);
-		p4.translate(dx, dy);
+	    for (Point ball:balls){
+		ball.translate(dx, dy);
+	    }
 	}
 	
 	public void reInit() {
-		p1.setLocation(5, 8);
-		p2.setLocation(21, 1);
-		p3.setLocation(0, 4);
-		p4.setLocation(4, 8);
+	    for(int i=0; i< balls.length; i++){
+		this.balls[i] = this.ballsOri[i].getLocation();
+	    }
 	}
 	
 	@Override
 	public String toString() {
-		return "p1("+p1.getX()+";"+p1.getY()+"), p2("+p2.getX()+";"+p2.getY()+"), p3("+p3.getX()+";"+p3.getY()+"), p4("+p4.getX()+";"+p4.getY()+")";
+	    StringBuffer sb = new StringBuffer();
+	        sb.append("Il y a " + balls.length +" balles : \n");
+	        for(Point ball:balls){
+	            sb.append(ball.getX() + ", ");
+	            sb.append(ball.getY() + "\n");
+	        }
+	    return sb.toString(); 
+		//return "p1("+p1.getX()+";"+p1.getY()+"), p2("+p2.getX()+";"+p2.getY()+"), p3("+p3.getX()+";"+p3.getY()+"), p4("+p4.getX()+";"+p4.getY()+")";
 	}
-	
 	
 	
 }
