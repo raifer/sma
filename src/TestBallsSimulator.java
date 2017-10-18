556d1bfdb55778cccb4d64a8bbc0f7ea0d4f1d9a
Mathieu Barbe <mat.barbe@gmail.com>
2017-10-18 11:10:17 +0200

    Mise au point du teste : ça marche

--- src/TestBallsSimulator.java
+++ src/TestBallsSimulator.java
@@ -1,12 +1,18 @@
 import gui.GUISimulator;
-
 import java.awt.Color;
 
 public class TestBallsSimulator {
 
-	public static void main(String[] args) {
-		GUISimulator gui = new GUISimulator(500, 500, Color.BLACK);
-		gui.setSimulable(new BallsSimulator());
-	}
+    public static void main(String[] args) {
+        GUISimulator gui = new GUISimulator(500, 500, Color.BLACK);
+        gui.setSimulable(new BallsSimulator(gui));
+/*
+        for (int i = 100 ; i < 500 ; i += 100) {
+            gui.addGraphicalElement(
+                    new Oval(i, 250,
+                            Color.decode("#1f77b4"), Color.decode("#1f77b4"), 10)) ;
+        }
+*/
+}
 
 }
