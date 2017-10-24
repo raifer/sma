fd0e71e7e1e4bd5d31811091a8e51f8e74ef45e5
Claire <claire.deshayes71@gmail.com>
2017-10-24 08:24:34 +0200

    Ajout des classes Grille, Cellule et de l'énumération Etat

---
+++ src/Cellule.java
@@ -0,0 +1,14 @@
+
+
+public class Cellule {
+
+	
+	private Etat etat;
+	
+	public Etat getEtat() {
+		return this.etat;
+	}
+	public void setEtat(Etat e){
+		this.etat = e;
+	}
+}
---
+++ src/Etat.java
@@ -0,0 +1,4 @@
+
+public enum Etat {
+	VIVANT,MORT
+}
