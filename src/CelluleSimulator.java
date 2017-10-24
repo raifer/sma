57291e2414c1fd9720b3610522b6334185553ef8
Valentin Picard <picardv@ensipc23.imag.fr>
2017-10-24 17:03:25 +0200

    Mise à jour + finalisation de la classe CelluleSimulation

--- src/CelluleSimulator.java
+++ src/CelluleSimulator.java
@@ -1,5 +1,4 @@
 
-import java.awt.Point;
 import gui.GUISimulator;
 import gui.Simulable;
 import gui.Rectangle;
@@ -13,33 +12,37 @@ public class CelluleSimulator implements Simulable {
      * Création des Cellules et placement de celles-ci dans la GUI
      *** 
      * @param gui L'interface où va se dérouller la simulation
-     * @author Valentin
+     * @author picardv
      */
     public CelluleSimulator(GUISimulator gui){
         this.gui = gui;
         this.notreJeu = new JeuDeLaVie();
         this.drawJDLV();
     }
+    
+    /**
+     * Dessine les cellules dans notre afficheur graphique
+     */
 
     protected void drawJDLV(){
         gui.reset();
-        for (int i=0; i< this.notreJeu.getNbLignes(); i++){
-        	//on parcourt les lignes de la grille
-        	for (int j=0; j< this.notreJeu.getNbColonnes(); j++){
-        		//on parcourt les colonnes de la grille
+        for (int i=0; i< this.notreJeu.getNbColonnes(); i++){
+        	//on parcourt les Colonnes de la grille
+        	for (int j=0; j< this.notreJeu.getNbLignes(); j++){
+        		//on parcourt les Lignes de la grille
         		
         		Cellule C = this.notreJeu.getCellule(i,j);
 	            if (C.estVivante()){	
 	            	//Si la cellule est vivante, elle sera affichée en bleu
 		            gui.addGraphicalElement(
-		                    new Rectangle((int)C.getX(), (int)C.getY(),
+		                    new Rectangle((C.getXInt())*20,(C.getYInt())*20,
 		                            Color.decode("#000000"), Color.decode("#1f77b4"), 
 		                            20, 20));
 	            }
 	            else{
 	            	//Si la cellule est morte, elle sera affichée en blanc
 		            gui.addGraphicalElement(
-		                    new Rectangle((int)C.getX(), (int)C.getY(),
+		                    new Rectangle((C.getXInt())*20,(C.getYInt())*20,
 		                            Color.decode("#000000"), Color.decode("#ffffff"), 
 		                            20, 20));
 	            }
@@ -47,15 +50,23 @@ public class CelluleSimulator implements Simulable {
         }
     }
 
+    /**
+     * Mise à jour des cellule après un pas
+     * puis affichage
+     */
     @Override
     public void next() {
-        /*fonction de mise à jour de la grille*/;
+        notreJeu.majJeux();
         this.drawJDLV();
     }
 
+    	/**
+    	 * Réinitialisation des cellules
+    	 * puis affichage
+    	 */
     @Override
     public void restart() {
-        notreJeu.reInit();
+        //notreJeu.reInit();
         this.drawJDLV();
     }
 }
