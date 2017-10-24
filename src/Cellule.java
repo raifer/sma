ea9019327ad9ba3f65df81477cad0d6c8c2c4961
Claire <claire.deshayes71@gmail.com>
2017-10-24 12:24:57 +0200

    ajout de la méthode estVivante()

--- src/Cellule.java
+++ src/Cellule.java
@@ -1,14 +1,29 @@
+import java.awt.Point;
 
+public class Cellule extends Point {
 
-public class Cellule {
-
-	
 	private Etat etat;
 	
+	// Les get set
 	public Etat getEtat() {
 		return this.etat;
 	}
 	public void setEtat(Etat e){
 		this.etat = e;
+	}
+	
+	
+	
+	// Le constructeur
+	
+	
+	// Les méthodes
+	
+	public boolean estVivante() {
+		if (this.getEtat() == Etat.VIVANT){
+			return true;
+		}
+		else
+			return false;
 	}
 }
