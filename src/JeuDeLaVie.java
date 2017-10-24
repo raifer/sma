41e7c2ae926bd438d98d666f0edc1390a18fdb26
Claire <claire.deshayes71@gmail.com>
2017-10-24 09:26:31 +0200

    Changment de nom Grille - JeuDeLaVie

---
+++ src/JeuDeLaVie.java
@@ -0,0 +1,27 @@
+
+public class JeuDeLaVie {
+	public static final int NB_LIGNES = 10;
+	public static final int NB_COLONNES = 10;
+	
+	private Cellule[][] grille_cour = new Cellule[NB_LIGNES][NB_COLONNES];
+	private Cellule[][] grille_suiv = new Cellule[NB_LIGNES][NB_COLONNES];
+	
+
+	//Attention ne contient que des références nulles
+	
+	//A mettre dans le constructeur avec random
+	//Faire une boucle qui copie le tableau : voir balls.Ori
+	//Un tableau t un tableau t+1
+	//Claire : Faire le constructeur 
+	
+	//Méthode qui initialise la grille en affectant aléatoirement les états des cellules
+	
+		
+	//méthode 
+	}
+	
+	
+	
+	
+	
+}
