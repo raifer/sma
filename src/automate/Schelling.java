be6ed37283a7b2dfb96f15b334c82f693a4e251b
Mathieu Barbe <mat.barbe@gmail.com>
2017-10-29 23:48:08 +0100

    Ajout de nombresunombreuse fonction dans Schelling

--- src/automate/Schelling.java
+++ src/automate/Schelling.java
@@ -8,6 +8,12 @@ package automate;
  *
  */
 
+import java.util.LinkedList;
+import java.util.List;
+//import java.util.Iterator;
+import java.util.Random;
+
+
 public class Schelling extends AutomateCellulaire {
 
 	/** 
@@ -15,39 +21,71 @@ public class Schelling extends AutomateCellulaire {
 	 * 
 	 * Si une famille de couleur c a plus de K voisins de couleur différente alors la famille déménage, 
 	 */
-	int k = 3;
+	int k;
 	
 /**
  * Nombre de couleur pour la simulation 
  */
-private int nbCouleur = 3;
+private int nbCouleurs;
 
-	/**
- * @return the k
+/**
+ * Nombre de cellules vacantes dans la grille
  */
-public int getK() {
-	return k;
+private int nbCellulesVacantes;
+
+/**
+ * list qui contient toutes les cellules vacantes
+ * 
+ * Choix d'd'une liste chaîné car :
+ * - Pas d'accès concurant;
+ * - Besoin d'accès par indice.
+ */
+List<Cellule> cellulesVacantes = new LinkedList<Cellule>();
+
+
+/**
+ *List qui va contenir les nouvelles cellules vaccantes 
+ */
+List<Cellule> newCellulesVacantes = new LinkedList<Cellule>();
+
+private Random randomGenerator;
+
+public Schelling() {
+	super();
+	this.setK(3);
+	this.setNbCouleurs(3);
+	this.setNbCellulesVacantes(20);
+	this.randomGenerator = new Random();
+	this.placeCellulesVacantes(nbCellulesVacantes);
 }
 
 /**
  * @param k the k to set
  */
-public void setK(int k) {
+private void setK(int k) {
 	this.k = k;
 }
 
 /**
  * @return the nbCouleur
  */
-public int getNbCouleur() {
-	return nbCouleur;
+public int getNbCouleurs() {
+	return nbCouleurs;
 }
 
 /**
  * @param nbCouleur the nbCouleur to set
  */
-public void setNbCouleur(int nbCouleur) {
-	this.nbCouleur = nbCouleur;
+private void setNbCouleurs(int nbCouleur) {
+	this.nbCouleurs = nbCouleur;
+}
+
+	public int getNbCellulesVacantes() {
+	return nbCellulesVacantes;
+}
+
+private void setNbCellulesVacantes(int nbCellulesVacantes) {
+	this.nbCellulesVacantes = nbCellulesVacantes;
 }
 
 	/* (non-Javadoc)
@@ -55,14 +93,52 @@ public void setNbCouleur(int nbCouleur) {
 	 */
 	@Override
 	EtatS randomEtat() {
-		double p;
-		p = Math.random() * this.nbCouleur;
-		for (int i=1; i<= this.nbCouleur; i++) {
-		if (p < i) break;
-		}
-		return EtatS.valueOf(1);
+		int index = this.randomGenerator.nextInt(this.nbCouleurs-1)+1; // On ne veut pas 0 (vacante)
+		return EtatS.valueOf(index);
 	}
-
+	
+	/**
+	 * Placer un nombre spécifique de cellules vacantes sur la grille courante
+	 *  
+	 * @param nbVacantes Nombre de cellule vacantes à placer sur la grille
+	 */
+	private void placeCellulesVacantes(int nbVacantes) {
+		for(int i=0; i < nbVacantes; i++) {
+			this.placeRandomCelluleVacante();
+		}
+	}
+	
+	/**
+	 * Remplace une cellule occupée choisie au hasard par une vacante 
+	 */
+	private void placeRandomCelluleVacante() {
+		Cellule c;
+		int x, y;
+		// On cherche une cellule occupé pour la remplacer
+		do {
+			x = this.randomGenerator.nextInt(this.getNbColonnes());
+			y = this.randomGenerator.nextInt(this.getNbLignes());
+			c = super.getCellule(x, y);
+		} while (!c.estVivante());
+	super.setEtatCourant(x, y, EtatS.Vacante);
+	this.cellulesVacantes.add(new Cellule(x, y, EtatS.Vacante));
+	}
+	
+	private void demenagement(Cellule c) {
+		// On cherche une cellule vacante
+		int index = this.randomGenerator.nextInt(this.cellulesVacantes.size());
+		Cellule cible = this.cellulesVacantes.get(index);
+		int x = cible.getXInt();
+		int y = cible.getYInt();
+		// On supprim la place libre
+this.cellulesVacantes.remove(index);
+// On déplace la famille
+super.setEtatSuiv(x, y, c.getEtat());
+// On libère une cellule pour le tour suivant
+this.newCellulesVacantes.add(new Cellule(c.getXInt(), c.getYInt(), EtatS.Vacante));
+	}
+		
+	
 	/* (non-Javadoc)
 	 * @see automate.AutomateCellulaire#majCellule(automate.Cellule)
 	 */
