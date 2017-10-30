7f75e7f57f521ced88cca81dd2e6b766ec1a1476
Mathieu Barbe <mat.barbe@gmail.com>
2017-10-30 20:35:03 +0100

    Ajout de la class pour simuller le JeuDeLaVie

---
+++ src/automate/JeuDeLaVieSimulator.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package automate;
+
+import gui.GUISimulator;
+
+/**
+ * @author Mathieu
+ *
+ */
+public class JeuDeLaVieSimulator extends AutomateSimulator<JeuDeLaVie> {
+
+	/**
+	 * Création de l'automate JeuDeLaVie puis création de la grille
+	 * 
+	 * @param gui Interface graphique pour afficher notre simulation
+	 */
+	public JeuDeLaVieSimulator(GUISimulator gui) {
+		super(new JeuDeLaVie(), gui);
+	}
+
+}
