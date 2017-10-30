76034cc7f7295fe45f4cb02f35fb437bddf09246
Mathieu Barbe <mat.barbe@gmail.com>
2017-10-30 20:37:34 +0100

    Adaptation du test JDLV pour la nouvelle archi

--- src/automate/TestJDLVSimulator.java
+++ src/automate/TestJDLVSimulator.java
@@ -8,10 +8,8 @@ import gui.GUISimulator;
 public class TestJDLVSimulator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		GUISimulator gui = new GUISimulator(500, 500, Color.BLACK);
-		JeuDeLaVie monAuto = new JeuDeLaVie();
-        gui.setSimulable(new AutomateSimulator<JeuDeLaVie>(monAuto,  gui));
+        gui.setSimulable(new JeuDeLaVieSimulator(gui));
 	}
 
 }
