42670754a9c5231c9a3773559a68c486909ce4f4
Mathieu Barbe <mat.barbe@gmail.com>
2017-11-02 16:45:19 +0100

    Corrctioncorrection indentatation

--- src/boids/FlockSimulator.java
+++ src/boids/FlockSimulator.java
@@ -15,52 +15,52 @@ import boids.util.Vector;
  */
 
 public class FlockSimulator implements Simulable {
-private Flock flock;
-    
-protected GUISimulator gui;
-    
-    public FlockSimulator(GUISimulator gui){
-        this.gui = gui;
-        this.flock = new Flock();
-        this.drawFlock();
-    }
-    
-    /**
-     * Dessine les boids dans notre afficheur graphique
-     */
-    protected void drawFlock(){
-        gui.reset();
-        for (Boid boid : flock.getBoids()) {
-        		Vector c = boid.getPosition();
-        		gui.addGraphicalElement(
-	                    new Rectangle(
-	                    		(int)c.getX(),
-	                    		(int)c.getY(), 
-	                            Color.black, 
-	                            Color.red, 
-	                            50, 
-	                            50));
-        }
-    }
+	private Flock flock;
 
-    /**
-     * Mise à jour des boids après un pas
-     * puis affichage
-     */
-    @Override
-    public void next() {
-        this.flock.updateBoidsPostion();
-        this.drawFlock();
-    }
+	protected GUISimulator gui;
 
-    	/**
-    	 * Réinitialisation des cellules
-    	 * puis affichage
-    	 */
-    @Override
-    public void restart() {
-        this.flock.reInit();
-        this.drawFlock();
-    }
+	public FlockSimulator(GUISimulator gui){
+		this.gui = gui;
+		this.flock = new Flock();
+		this.drawFlock();
+	}
+
+	/**
+	 * Dessine les boids dans notre afficheur graphique
+	 */
+	protected void drawFlock(){
+		gui.reset();
+		for (Boid boid : flock.getBoids()) {
+			Vector c = boid.getPosition();
+			gui.addGraphicalElement(
+					new Rectangle(
+							(int)c.getX(),
+							(int)c.getY(), 
+							Color.black, 
+							Color.red, 
+							50, 
+							50));
+		}
+	}
+
+	/**
+	 * Mise à jour des boids après un pas
+	 * puis affichage
+	 */
+	@Override
+	public void next() {
+		this.flock.updateBoidsPostion();
+		this.drawFlock();
+	}
+
+	/**
+	 * Réinitialisation des cellules
+	 * puis affichage
+	 */
+	@Override
+	public void restart() {
+		this.flock.reInit();
+		this.drawFlock();
+	}
 }
 
