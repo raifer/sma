44f9a75419476fedb8e32c0c47ef03d9cdbcb8e1
Mathieu Barbe <mat.barbe@gmail.com>
2017-11-07 11:41:19 +0100

    Ajout de code dans le test de Flock

--- src/boids/TestFlock.java
+++ src/boids/TestFlock.java
@@ -15,6 +15,9 @@ public class TestFlock {
 	public static void main(String[] args) {
 Flock flock = new Flock(200, 100, 2);
 System.out.println(flock);
+System.out.println("Step 1");
+flock.updateBoidsPostion();
+System.out.println(flock);
 	}
 
 }
