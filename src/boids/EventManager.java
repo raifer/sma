ebea4e23ca51518645f566ae789236df4dd48f51
Claire <claire.deshayes71@gmail.com>
2017-11-07 16:17:04 +0100

    Ajouts ddu tableau d'évenements et du pas de temps

--- src/boids/EventManager.java
+++ src/boids/EventManager.java
@@ -3,7 +3,8 @@ package boids;
 class EventManager {
 	//Variables
 	private long currentDate;
-	private Event[] EventSequence; 
+	private int pas;
+	private Event[] eventSequence; 
 	
 	//Get/Set
 	public long getCurrentDate() {
@@ -12,15 +13,25 @@ class EventManager {
 	public void setCurrentDate(long date) {
 		this.currentDate = date;
 	}
+	public int getPas() {
+		return this.pas;
+	}
+	public void setPas(int p) {
+		this.pas = p;
+	}
 	
 	//Constructeur
 	public EventManager(long date) {
 		setCurrentDate(date);
 	}
 	
-	public void addEvent(e : Event) {
-		
+	public void addEvent(Event e) {
+		//this.eventSequence[e.getDate()]=e; Comment indexer un tableau en long et non en int ?
 	}
+	
+	/*
+	 * On exécut les évènements à réaliser en un pas de temps
+	 */
 	public void next() {
 		
 	}
