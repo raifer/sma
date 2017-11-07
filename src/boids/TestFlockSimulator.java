9d9b5dccad8d6b788072d711b0e98cac59014558
Mathieu Barbe <mat.barbe@gmail.com>
2017-11-07 11:42:25 +0100

    Modification de la taille de la simu

--- src/boids/TestFlockSimulator.java
+++ src/boids/TestFlockSimulator.java
@@ -18,9 +18,9 @@ public class TestFlockSimulator {
 	 */
 	public static void main(String[] args) {
 		
-		int height = 1500;
-		int width = 2000;
-		GUISimulator gui = new GUISimulator(height, width, Color.BLACK);
+		int height = 1000;
+		int width = 1600;
+		GUISimulator gui = new GUISimulator(width, height, Color.BLACK);
 FlockSimulator flockSimu = new FlockSimulator(gui);
         gui.setSimulable(flockSimu);
 
