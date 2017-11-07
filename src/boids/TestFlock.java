624d63f5f7c889c5212669dc98bd0a991e041188
Mathieu Barbe <mat.barbe@gmail.com>
2017-11-07 10:58:48 +0100

    Ajout d'une calssclass de teste

---
+++ src/boids/TestFlock.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package boids;
+
+/**
+ * @author Mathieu
+ *
+ */
+public class TestFlock {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+Flock flock = new Flock(200, 100, 2);
+System.out.println(flock);
+	}
+
+}
