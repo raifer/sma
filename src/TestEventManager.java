f6de29427eca8d49e6199e3cc19694c53c3346a7
Claire <claire.deshayes71@gmail.com>
2017-11-13 16:09:50 +0100

    Remise du test PING PONG

--- src/TestEventManager.java
+++ src/TestEventManager.java
@@ -5,14 +5,17 @@ public class TestEventManager {
 
 		EventManager manager = new EventManager();
 		
-		Event e1 = new MessageEvent(1,"hello");
-		Event e2 = new MessageEvent(2, "hola");
 		
-		if(e1<e2) {
-			System.out.println("1<2");
+		for (int i = 2; i<= 10; i+=2) {
+			manager.addEvent(new MessageEvent(i, "[PING]"));
 		}
-		else 
-			System.out.println("2<1");
+		for (int i = 3; i<= 9; i+=3) {
+			manager.addEvent(new MessageEvent(i, "[PONG]"));
+		}
+		
+		while (!manager.isFinished()) {
+			manager.next();
+		}
 	}
 
 }
